ce35eeafe9dd8eeb0ea78001487642b857e0bb93
verysadbird <77414505+verysadbird@users.noreply.github.com>
2021-01-19 16:48:17 +0300

    Add project files.

---
+++ CustomerManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+
+        public static void Add (Customer c)
+        {
+            Console.WriteLine("Customer added! : " + c.Name + " "  + c.Surname);
+        }
+
+        public static void Delete (Customer c)
+        {
+            Console.WriteLine("Customer deleted! : " + c.Name + " " + c.Surname);
+        }
+
+        public static void List (Customer[] c)
+        {
+            Console.WriteLine("Customer List:\n");
+            Console.WriteLine("Name        Surname            ID");
+            foreach (Customer cs in c)
+            {
+                Console.WriteLine(cs.Name + "           " + cs.Surname + "             " + cs.ID);
+               
+            }
+            
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Customer a = new Customer();
+            a.ID = 101;
+            a.Name = "John";
+            a.Surname = "Doe";
+
+            Customer b = new Customer();
+            b.ID = 102;
+            b.Name = "Jane";
+            b.Surname = "Doe";
+
+            Customer c = new Customer();
+            c.ID = 103;
+            c.Name = "John";
+            c.Surname = "Smith";
+
+            Customer[] cs = new Customer[] {a, b, c};
+
+            CustomerManager.Add(a);
+            CustomerManager.Add(b);
+            CustomerManager.Add(c);
+            CustomerManager.Delete(a);
+            CustomerManager.Delete(b);
+            CustomerManager.Delete(c);
+
+            Console.WriteLine();
+            Console.WriteLine("---------------------------");
+            Console.WriteLine();
+            CustomerManager.List(cs);
+
+
+        }
+    }
+}
